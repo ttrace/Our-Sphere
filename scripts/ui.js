50c294d5babac13f544556aaee266358ac841890
Taiyo FUJII <taiyoster@gmail.com>
2012-04-17 09:42:11 +0900

    night mode stability, object devided from daytime mapping

---
+++ scripts/ui.js
@@ -0,0 +1,6 @@
+function ui_init(){
+     var screen_size = [ window.innerWidth , window.innerHeight ]
+     var my_view = document.getElementById("viewer");
+//          my_view.style.width = screen_size[0] + "px";
+//          my_view.style.height = screen_size[1] + "px";
+}
